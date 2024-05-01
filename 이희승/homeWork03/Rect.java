9594ad7472d299b721bd26b773ff9472e8881f30
leeheeseng <red991018@gmail.com>
2024-05-01 21:41:43 +0900

    이희승 브랜치생성 과 과제미들까지

---
+++ 이희승/homeWork03/Rect.java
@@ -0,0 +1,35 @@
+package homeWork.homeWork03;
+
+public class Rect {
+
+	public int width;
+	public int hight;
+	
+	public Rect() {
+		
+	} 
+	
+	public Rect(int width,int hight) {
+		this.width=width;
+		this.hight=hight;
+	}
+	
+	public  int rA() {
+		return 2*(width+hight);
+	}
+	
+	public int ra() {
+		return width*hight;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
