bdaaf9e6812fed02775807e299cead01e11c2c10
jangho309 <jangho309@naver.com>
2024-05-08 21:07:46 +0900

    24.05.08 homework1~7 폴더 재수정 및 새로 업로드

--- 조장호/homework04/advance/ArrayUtility2.java
+++ 조장호/homework04/advance/ArrayUtility2.java
@@ -7,12 +7,15 @@ public class ArrayUtility2 {
 		int[] inputArr1 = new int[] {1, 2, 3, 4, 5, 6, 7, 8};
 		int[] inputArr2 = new int[] {3, 6, 8};
 		
+		int[] inputArr3 = new int[] {1, 2, 3, 4, 5, 1, 2, 3};
+		int[] inputArr4 = new int[] {1, 1, 1, 1, 2, 2, 1, 3, 2, 1, 3, 2};
 //		int[] concatResult = concat(inputArr1, inputArr2);
 //		for(int a = 0; a < concatResult.length; a++) {
 //			System.out.println(a + "번째 값 : " + concatResult[a]);
 //		}
 		System.out.println("----------------------------------------");
-		int[] removeResult = remove(inputArr1, inputArr2);
+//		int[] removeResult = remove(inputArr1, inputArr2);
+		int[] removeResult = remove(inputArr3, inputArr4);
 		for(int b = 0; b < removeResult.length; b++) {
 			System.out.println(b + "번째 값 : " + removeResult[b]);
 		}
@@ -29,39 +32,70 @@ public class ArrayUtility2 {
 		return concateArr;
 	};
 	// s1에서 s2배열의 숫자를 모두 삭제한 새로운 배열 리턴
-	// {1, 2, 3, 4, 5, 6, 7, 8},     {3, 6, 8} -> {1, 2, 4, 5, 7}
-	// {1, 2, 0, 4, 5, 0, 7, 0}
-	// {X, X, X, X, X}
+	// s1 : {1, 2, 3, 4, 5, 6, 7, 8},    s2: {3, 6, 8} ->  결과 : {1, 2, 4, 5, 7}
 	static int[] remove(int[] s1, int[] s2) {
-		int[] isExistedArr = new int[s1.length];
-		int isCount = 0;
-		for(int j = 0; j < s2.length; j++) {
+		int[] isExistedArr;
+		int[] rtnRemoveArr;
+		if(s1.length >= s2.length) {
+			isExistedArr = new int[s1.length];
+			int isCount = 0;
+			for(int j = 0; j < s2.length; j++) {
+				for(int i = 0; i < s1.length; i++) {
+					if(s1[i] == s2[j]) {
+						isExistedArr[i] = s1[i];
+						isCount++;
+					} 
+				}
+			}
+			for(int a = 0; a < isExistedArr.length; a++) {
+				System.out.println("isExistedArr[" + a + "] = " + isExistedArr[a]);
+			}
+			System.out.println("체크된 값 개수 : " + isCount);
+			
+			// s1의 배열길이 - 중복된 값의 개수로 새로 배열 생성
+			rtnRemoveArr = new int[s1.length - isCount];
+			// 중복된 값이 없을 때 인덱스를 추가하는 값
+			int passIndex = 0;
+			for(int k = 0; k < s1.length; k++) {
+				if(isExistedArr[k] == 0) {
+					rtnRemoveArr[passIndex++] = s1[k];
+				}
+				if(passIndex == s1.length - isCount) {
+					break;
+				}
+			}
+		// s1 : {1, 2, 3},    s2: {3, 4, 5, 6, 7, 8, 9} ->  결과 : {1, 2}
+		} else {
+			isExistedArr = new int[s1.length];
+			int isCount = 0;
+			
+			// i -> s1, j -> s2
 			for(int i = 0; i < s1.length; i++) {
-				if(s1[i] == s2[j]) {
-					isExistedArr[i] = s1[i];
-					isCount++;
-				} 
+				for(int j = 0; j < s2.length; j++) {
+					if(s1[i] == s2[j]) {
+						
+					}
+				}
 			}
-		}
-		for(int a = 0; a < isExistedArr.length; a++) {
-			System.out.println("isExistedArr[" + a + "] = " + isExistedArr[a]);
-		}
-		System.out.println("체크된 값 개수 : " + isCount);
-		
-		int[] rtnRemoveArr = new int[s1.length - isCount];
-		int passIndex = 0;
-		for(int k = 0; k < s1.length; k++) {
-			if(isExistedArr[k] == 0) {
-				rtnRemoveArr[passIndex++] = s1[k];
+			
+			for(int a = 0; a < isExistedArr.length; a++) {
+				System.out.println("isExistedArr[" + a + "] = " + isExistedArr[a]);
 			}
-			if(passIndex == s1.length - isCount) {
-				break;
-			}
+			System.out.println("체크된 값 개수 : " + isCount);
+			
+			// s1의 배열길이 - 중복된 값의 개수로 새로 배열 생성
+			rtnRemoveArr = new int[s1.length - isCount];
+//			// 중복된 값이 없을 때 인덱스를 추가하는 값
+//			int passIndex = 0;
+//			for(int k = 0; k < s1.length; k++) {
+//				if(isExistedArr[k] == 0) {
+//					rtnRemoveArr[passIndex++] = s1[k];
+//				}
+//				if(passIndex == s1.length - isCount) {
+//					break;
+//				}
+//			}
 		}
-		// s1 			:	{1, 2, 3, 4, 5, 6, 7, 8}
-		// isExistedArr : 	{0, 0, 3, 0, 0, 6, 0, 8}
-		// {1, 2, 4, 5, 7}
-		
 		return rtnRemoveArr;
-	};
+	}
 }
