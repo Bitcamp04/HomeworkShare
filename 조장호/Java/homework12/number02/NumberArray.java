236f649868df8b7f6a499b2940720c697cd44096
jangho309 <jangho309@naver.com>
2024-05-24 20:23:34 +0900

    24.05.24 자바 homework12 클래스문제까지 업로드

--- 조장호/Java/homework12/number02/NumberArray.java
+++ 조장호/Java/homework12/number02/NumberArray.java
@@ -94,9 +94,69 @@ public class NumberArray {
 	public static void numberArraySix(Scanner sc) {
 		List<String> strList = new ArrayList<String>();
 		
-		for(int i = 1; i < 5; i++) {
+		for(int i = 1; i <= 5; i++) {
 			System.out.println(i + "번째 문자열을 입력해 주세요.");
-			
+			strList.add(sc.nextLine());
 		}
+		
+		String longestStr = strList.get(0);
+		String shortestStr = strList.get(0);
+		for(int j = 1; j < strList.size(); j++) {
+			if(longestStr.length() < strList.get(j).length()) {
+				longestStr = strList.get(j);
+			}
+			
+			if(shortestStr.length() > strList.get(j).length()) {
+				shortestStr = strList.get(j);
+			}
+		}
+		
+		System.out.println("입력하신 문자열 중 가장 긴 문자열 : " + longestStr);
+		System.out.println("입력하신 문자열 중 가장 짧은 문자열 : " + shortestStr);
+	}
+	
+	// 7. 두 개의 정수형 배열을 만들고 1 ~ 100까지의 랜덤한 값으로 첫 번째 배열은 4개 두 번째 배열은 3개를 저장하고 두 배열을 합친 새로운 배열을 생성하세요.
+	public static void numberArraySeven() {
+		int[] intArr1 = new int[4];
+		int[] intArr2 = new int[3];
+		
+		for(int i = 0; i < intArr1.length; i++) {
+			intArr1[i] =  (int)(Math.random() * 100) + 1;
+		}
+		System.out.println("첫 번째 배열 : " + Arrays.toString(intArr1));
+		for(int i = 0; i < intArr2.length; i++) {
+			intArr2[i] =  (int)(Math.random() * 100) + 1;
+		}
+		System.out.println("두 번째 배열 : " + Arrays.toString(intArr2));
+		int[] concatArr = new int[7];
+		for(int j = 0; j < intArr1.length; j++) {
+			concatArr[j] = intArr1[j];
+		}
+		
+		for(int k = intArr1.length; k < intArr1.length + intArr2.length; k++) {
+			concatArr[k] = intArr2[k - intArr1.length];
+		}
+		
+		System.out.println("합친 배열 : " + Arrays.toString(concatArr));
+	}
+	
+	// 8. 사용자가 문자열을 입력하는데 문자열 사이사이에 -을 넣어 구분자로 만들어준다. -기준으로 잘라진 문자열 배열을 생성하고 잘라진 문자열 중에
+    // 가장 길이가 긴 문자열의 인덱스와 그 문자열을 출력하세요.
+	public static void numberArrayEigth(Scanner sc) {
+		System.out.println("문자열을 입력해주세요. -문자는 구분자로 사용됩니다.");
+		String inputStr = sc.nextLine();
+		
+//		System.out.println("첫 하이픈 인덱스 : " + inputStr.indexOf("-") + ", 마지막 하이픈 인덱스 : " + inputStr.lastIndexOf("-"));
+//		System.out.println(inputStr.indexOf("-", inputStr.indexOf("-") + 1));
+		
+		String[] splitedStr = inputStr.split("-");
+		String longestStr = splitedStr[0];
+		for(int i = 1; i < splitedStr.length; i++) {
+			if(longestStr.length() < splitedStr[i].length()) {
+				longestStr = splitedStr[i];
+			}
+		}
+		
+		System.out.println("가장 길이가 긴 문자열의 인덱스 : " + inputStr.indexOf(longestStr) + ", 그 문자열 : " + longestStr);
 	}
 }
